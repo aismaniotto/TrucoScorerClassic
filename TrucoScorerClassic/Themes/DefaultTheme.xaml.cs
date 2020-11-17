55913c627e083c4194e78f54c9262778d10e0373
aismaniotto <aismaniotto@gmail.com>
2020-11-16 21:30:23 -0300

    style: add themes and create default (brown schema)

---
+++ TrucoScorerClassic/Themes/DefaultTheme.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace TrucoScorerClassic.Themes
+{
+    public partial class DefaultTheme : ResourceDictionary
+    {
+        public DefaultTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
