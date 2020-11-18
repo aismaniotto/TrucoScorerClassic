b0c9b9cd0a4d1610c8afd8e54d008f0803388af1
aismaniotto <aismaniotto@gmail.com>
2020-11-17 21:35:25 -0300

    style: add icon to android app

--- TrucoScorerClassic.Android/MainActivity.cs
+++ TrucoScorerClassic.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace TrucoScorerClassic.Droid
 {
-    [Activity(Label = "Truco", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "Truco", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
